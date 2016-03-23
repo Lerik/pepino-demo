3985b730dcbe5e04472a9ab6c62da52cd5f0d258
Byron Sommardahl <byron@acklenavenue.com>
2016-03-23 16:23:32 -0600

    Working!

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var pepino = require('pepino-lib');
+var router = express.Router();
+
+var sample = `I have navigated to the search screen
+  Navigate to http://mysite.com
+
+I search for "$SearchTerm"
+  Type "$SearchTerm" into <#search> element
+  Click the <#submit> button
+
+I should find "$ExpectedResult" in the results
+  Verify "$ExpectedResult" is in <#results> element`;
+ 
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Pepino Demo', pepinoLang: sample });
+});
+
+router.post('/', function(req, res, next) {
+    var cucumberJs = pepino.converter(req.body.pepinoLang); 
+    console.log(cucumberJs);
+    res.render('index', { title: 'Pepino Demo', pepinoLang: sample, cucumberJs: cucumberJs });
+});
+
+module.exports = router;
