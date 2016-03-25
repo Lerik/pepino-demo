ce48c6c44e351753a65282d52c6c0973d43e1a2f
Byron Sommardahl <byron@acklenavenue.com>
2016-03-25 07:53:18 -0600

    Adding version number display.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var pepino = require('pepino-lib');
 var router = express.Router();
+var pkg = require('pepino-lib/package.json');
 
 var sample = `I have navigated to the search screen
   Navigate to http://mysite.com
@@ -13,14 +14,14 @@ I should find "$ExpectedResult" in the results
   Verify "$ExpectedResult" is in <#results> element`;
  
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Pepino Demo', pepinoLang: sample });
+    res.render('index', { title: 'Pepino Demo', pepinoLang: sample, version: pkg.version });
 });
 
 router.post('/', function(req, res, next) {
     var pepinoLang = req.body.pepinoLang;
     var cucumberJs = pepino.convert(pepinoLang); 
     console.log(cucumberJs);
-    res.render('index', { title: 'Pepino Demo', pepinoLang: pepinoLang || sample, cucumberJs: cucumberJs });
+    res.render('index', { title: 'Pepino Demo', pepinoLang: pepinoLang || sample, cucumberJs: cucumberJs, version: pkg.version });
 });
 
 module.exports = router;
